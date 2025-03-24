6d80d70d970a8c2ac5a7c6847a5ac31bbf8a4e69
tangT <770061208@qq.com>
2025-03-24 21:48:37 +0800

    .

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <h1>首页</h1>
+        <p>欢迎来到我的 Vue 应用</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+    padding: 20px;
+    text-align: center;
+}
+</style>
